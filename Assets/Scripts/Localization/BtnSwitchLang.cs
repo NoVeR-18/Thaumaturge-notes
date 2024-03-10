25953d91c15dd7cf922ba0d7d2a2bd9decee1b8c
Alexander <63400025+NoVeR-18@users.noreply.github.com>
2024-03-11 01:53:40 +0200

    scripts to support localization

---
+++ Assets/Scripts/Localization/BtnSwitchLang.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BtnSwitchLang : MonoBehaviour
+{
+    [SerializeField]
+    private LocalizationManager localizationManager;
+
+    public void OnButtonClick()
+    {
+        if (PlayerPrefs.GetString("Language").Equals("jp_JP"))
+        {
+            localizationManager.CurrentLanguage = "en_US";
+            localizationManager.LoadLocalizedText("en_US");
+        }
+        else if (PlayerPrefs.GetString("Language").Equals("en_US"))
+        {
+            localizationManager.CurrentLanguage = "jp_JP";
+            localizationManager.LoadLocalizedText("jp_JP");
+        }
+
+        //localizationManager.CurrentLanguage = name;
+    }
+}
